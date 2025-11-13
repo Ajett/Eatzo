4945a47b3b0041d37e0501ab7dcc36f3da7ff859
Ajeet Kumar <130174199+Ajett@users.noreply.github.com>
2025-11-13 19:46:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const Port = 5000;
 app.use(express.json());
 app.use(cors({
-origin:'http://localhost:5173'
+origin:['http://localhost:5173',"https://eatzo-1.onrender.com"]
 }))
 
 mongoose.connect('mongodb+srv://ak4882260_db_user:lKulZjoL6X1nRNY3@eatzo.f5kwpur.mongodb.net/').then(() => {
