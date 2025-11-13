4956ed977b4cffb8a75ddc0d682cc54f5a0fbcba
Ajeet Kumar <130174199+Ajett@users.noreply.github.com>
2025-11-13 20:08:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,50 @@
-// vite.config.js
+// // vite.config.js
 
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'; // You likely need this plugin for React!
+
+// export default defineConfig({
+//   plugins: [
+//     react(), // 👈 Add the React plugin if you are using React
+//     tailwindcss(),
+//   ],
+  
+//   // 👇 Add the server configuration to set up the proxy
+//   server: {
+//     proxy: {
+//       // All requests starting with '/mapi' (like /mapi/menu/pl?...) 
+//       // will be intercepted and proxied to the target URL.
+//       '/mapi': {
+//         target: 'https://www.swiggy.com',
+//         changeOrigin: true, // IMPORTANT: Changes the 'Origin' header to the target URL
+//         secure: true,       // Use HTTPS for the target
+//       },
+//     },
+//   },
+// })
+
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'; // You likely need this plugin for React!
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(), // 👈 Add the React plugin if you are using React
+    react(),
     tailwindcss(),
   ],
-  
-  // 👇 Add the server configuration to set up the proxy
+
   server: {
     proxy: {
-      // All requests starting with '/mapi' (like /mapi/menu/pl?...) 
-      // will be intercepted and proxied to the target URL.
-      '/mapi': {
+      // Swiggy actual working API (2025)
+      '/dapi': {
         target: 'https://www.swiggy.com',
-        changeOrigin: true, // IMPORTANT: Changes the 'Origin' header to the target URL
-        secure: true,       // Use HTTPS for the target
+        changeOrigin: true,
+        secure: true,
       },
     },
   },
-})
+});
